0cc823c0ccf389b65db838ba9993ac4f7a9ff58b
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-09-23 00:12:57 +0600

    Add files via upload

---
+++ Array/declare an Array.js
@@ -0,0 +1,10 @@
+var arr=[1,2,3,4,5,6,7,8,9]
+
+console.log(arr)
+console.log(arr[2])
+arr[2]=3100
+console.log(arr[2])
+console.log(arr.length)
+
+var arr2=Array(1,2,3,4,5)// constructor way
+console.log(arr2)
