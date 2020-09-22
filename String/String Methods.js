25ba67da2df1b27f7ffb1f09f6e3ed6072c25a61
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-09-22 20:06:43 +0600

    Add files via upload

---
+++ String/String Methods.js
@@ -0,0 +1,16 @@
+var a='I am '
+var b='Jack Pritom Soren'
+
+var c=a.concat('',b)
+console.log(c)
+var d=c.substr(5)
+console.log(d)
+
+console.log(c.charAt(0))
+console.log(c.startsWith('I am'))
+console.log(c.endsWith('Soren'))
+console.log(c.toUpperCase())
+console.log(c.toLowerCase())
+console.log('     abcdefg  '.trim())
+console.log(c.split(''))
+
