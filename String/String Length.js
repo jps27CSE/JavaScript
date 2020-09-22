fd0b93836abc5526318f3d5efc223bd77ecbffe2
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-09-22 23:44:20 +0600

    Add files via upload

---
+++ String/String Length.js
@@ -0,0 +1,19 @@
+var str="Jack Pritom Soren"
+
+var length=0
+
+while(true)
+{
+    if(str.charAt(length)=='')
+    {
+        break
+    }
+    else
+    {
+        length++
+    }
+}
+
+console.log(length)
+console.log(str.length)
+console.log('jack'.length)
