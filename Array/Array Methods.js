a30e66e6aebe2726ac798c335939b3eae68bf2de
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-09-26 15:36:01 +0600

    Add files via upload

---
+++ Array/Array Methods.js
@@ -0,0 +1,13 @@
+var arr=[1,2,3,4,5]
+var arr2=[70,80,90]
+
+console.log(arr.join(''))
+console.log(arr.join(','))
+
+//arr.fill(0)
+//console.log(arr)
+
+var arr3=arr.concat(arr2)
+console.log(arr3)
+
+console.log(Array.isArray(arr))
---
+++ Array/Multidimensional Array.js
@@ -0,0 +1,14 @@
+var arr=
+[
+    [10,20,30,40],
+    [100,200,300,400],
+    [50,60,55,47]
+]
+
+for(var i=0;i<arr.length;i++)
+{
+    for(var j=0;j<arr[i].length;j++)
+    {
+        console.log('Element '+i+':'+ arr[i][j])
+    }
+}
