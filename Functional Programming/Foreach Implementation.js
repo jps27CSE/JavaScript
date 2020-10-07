e421250d8547a721c9696e24264decdd9cfcd3c9
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-10-07 13:08:09 +0600

    Add files via upload

---
+++ Functional Programming/Foreach Implementation.js
@@ -0,0 +1,28 @@
+var arr=[1,2,3,4,5]
+
+
+function forEach(arr,cb)
+{
+    for(var i=0;i<arr.length;i++)
+    {
+        cb(arr[i],i,arr)
+    }
+}
+
+var sum=0
+
+forEach(arr,function(value,index,arr)
+{
+    console.log(value,index,arr)
+    sum+=value
+})
+
+console.log(sum)
+
+
+forEach(arr,function(value,index,arr)
+{
+    arr[index]=value+5
+})
+
+console.log(arr)
