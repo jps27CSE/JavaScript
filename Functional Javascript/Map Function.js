17fbd5886b89ab87dd8297be99612722674bdbdd
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-11-30 22:14:36 +0600

    Add files via upload

---
+++ Functional Javascript/Map Function.js
@@ -0,0 +1,8 @@
+var arr=[4,5,6,7,8,10,13]
+
+var newarr=arr.map(function(value)
+{
+    return value +2
+})
+
+console.log(newarr)
---
+++ Functional Javascript/Reduce Function.js
@@ -0,0 +1,9 @@
+var arr=[1,2,3,4,5]
+
+var result=arr.reduce(function(a,b){
+    return a*b
+})
+
+
+console.log(result)
+
