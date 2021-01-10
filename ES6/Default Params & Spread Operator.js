61d9c04125d6d4e6921f1c3eab78934698def927
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-01-11 01:50:03 +0600

    Add files via upload

---
+++ ES6/Default Params & Spread Operator.js
@@ -0,0 +1,15 @@
+function greet($greeting = 'hello world') {
+    console.log($greeting)
+}
+
+greet()
+
+
+let array = [1,2,3,4]
+
+function print()
+{
+    console.log(array)
+}
+
+print(...array)
---
+++ ES6/New String & Number Methods.js
@@ -0,0 +1,26 @@
+let Name = 'Jack Pritom Soren'
+
+console.log(Name.startsWith('Jack'))
+
+console.log(Name.endsWith('Soren'))
+
+console.log(Name.includes('Pritom'))
+
+//Hex 
+
+console.log(0xFF)
+
+//Binary
+
+console.log(0b101011)
+
+//Octal
+
+console.log(0o543)
+
+
+console.log(Number.isFinite(3))
+
+console.log(Number.isNaN(3))
+
+console.log(Number.isInteger(2))
